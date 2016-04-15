feb1919fc843922fafd3972dcafc6ff5460ab5df
Toby Chow <toby_chow@hotmail.com>
2016-04-15 23:00:03 +0800

    complete pomo clock, consider adding animations

--- timer.js
+++ timer.js
@@ -1,102 +1,144 @@
-// DRY
-
 $(document).ready(function() {
 
-    // Initial work minute / seconds
+    // Initial work minutes / seconds
 
-    var startWorkMinute = 0;
-    var startWorkSeconds = 3;
-    // Display initial time
+    var startWorkMinute = 25;
+    var startWorkSeconds = 0;
 
-    if (startWorkMinute <= 9) {
-        $("#work-minutes-display").html('0' + startWorkMinute);
-    } else {
-        $("#work-minutes-display").html(startWorkMinute);
-    }
-    if (startWorkSeconds <= 9) {
-        $("#work-seconds-display").html('0' + startWorkSeconds);
-    } else {
-        $("#work-seconds-display").html(startWorkSeconds);
-    }
+    // Initial break minutes / seconds
 
-    //work timer function
+    var startBreakMinute = 5;
+    var startBreakSeconds = 0;
 
-    function workCountDown() {
-        if (startWorkSeconds - 1 >= 0) {
-            if (startWorkSeconds - 1 <= 9) {
-                startWorkSeconds -= 1;
-                $("#work-seconds-display").html('0' + startWorkSeconds);
-            } else {
-                startWorkSeconds -= 1;
-                $("#work-seconds-display").html(startWorkSeconds);
-            }
-        } else if (startWorkMinute === 0 && startWorkSeconds === 0) {
-            //alert("done");
-            breakCountDown();
+    // COUNT TO STOP LOOP AFTER BREAK TIMER ENDS
+    var count = 1;
 
+    // PAUSE
+    var isPaused = true;
+
+
+    // ADD / MINUS INITIAL WORK/BREAK SESSION LENGTHS
+
+    $("#add-work-min").click(function() {
+        startWorkMinute += 1;
+        display();
+    });
+
+    $("#minus-work-min").click(function() {
+        if ($(".start-work-display").html() <= 1) {
+            startWorkMinute -= 0;
         } else {
-            startWorkSeconds = 59;
+            startWorkMinute -= 1;
+            display();
+        }
+    });
+
+    $("#add-break-min").click(function() {
+        startBreakMinute += 1;
+        display();
+    });
+
+    $("#minus-break-min").click(function() {
+        if ($(".start-break-display").html() <= 1) {
+            startBreakMinute -= 0;
+        } else {
+            startBreakMinute -= 1;
+            display();
+        }
+    });
+
+    // RESET BUTTON 
+
+    $("#reset").click(function() {
+        $("#status").html('Work Time!');
+        startWorkMinute = 25;
+        startWorkSeconds = 0;
+        startBreakMinute = 5;
+        startBreakSeconds = 0;
+        isPaused = true;
+        display();
+    });
+
+
+    // DISPLAYS INITIAL WORK/BREAK SESSION AND INITIAL START TIME FOR COUNTDOWN TIMER
+    function display() {
+        if (startWorkMinute <= 9) {
+            $("#work-minutes-display").html('0' + startWorkMinute);
+            $(".start-work-display").html('0' + startWorkMinute);
+        } else {
+            $("#work-minutes-display").html(startWorkMinute);
+            $(".start-work-display").html(startWorkMinute);
+        }
+        if (startWorkSeconds <= 9) {
+            $("#work-seconds-display").html('0' + startWorkSeconds);
+        } else {
             $("#work-seconds-display").html(startWorkSeconds);
-            if (startWorkMinute - 1 <= 9) {
-                startWorkMinute -= 1;
-                $("#work-minutes-display").html('0' + startWorkMinute);
-
-            } else {
-                startWorkMinute -= 1;
-                $("#work-minutes-display").html(startWorkMinute);
-            }
         }
-    }
-
-    var workTimer = setInterval(workCountDown, 1000);
-
-    // Initial break minute / seconds
-
-    var startBreakMinute = 1;
-    var startBreakSeconds = 3;
-    // Display initial time
-
-    if (startBreakMinute <= 9) {
-        $("#break-minutes-display").html('0' + startBreakMinute);
-    } else {
-        $("#break-minutes-display").html(startBreakMinute);
-    }
-    if (startBreakSeconds <= 9) {
-        $("#break-seconds-display").html('0' + startBreakSeconds);
-    } else {
-        $("#break-seconds-display").html(startBreakSeconds);
-    }
-
-
-    // break timer function
-
-    function breakCountDown() {
-        if (startBreakSeconds - 1 >= 0) {
-            if (startBreakSeconds - 1 <= 9) {
-                startBreakSeconds -= 1;
-                $("#break-seconds-display").html('0' + startBreakSeconds);
-            } else {
-                startBreakSeconds -= 1;
-                $("#break-seconds-display").html(startBreakSeconds);
-            }
-        } else if (startBreakMinute === 0 && startBreakSeconds === 0) {
-            //alert("done");
-            clearInterval(workTimer);
+        if (startBreakMinute <= 9) {
+            $(".start-break-display").html('0' + startBreakMinute);
         } else {
-            startBreakSeconds = 59;
-            $("#break-seconds-display").html(startBreakSeconds);
-            if (startBreakMinute - 1 <= 9) {
-                startBreakMinute -= 1;
-                $("#break-minutes-display").html('0' + startBreakMinute);
+            $(".start-break-display").html(startBreakMinute);
+        }
+    }
+    display();
 
+
+    //COUNTDOWN TIMER FUNCTION
+
+    function timer() {
+        if (!isPaused) {
+            if (startWorkSeconds - 1 >= 0) {
+                if (startWorkSeconds - 1 <= 9) {
+                    startWorkSeconds -= 1;
+                    $("#work-seconds-display").html('0' + startWorkSeconds);
+                } else {
+                    startWorkSeconds -= 1;
+                    $("#work-seconds-display").html(startWorkSeconds);
+                }
+            } else if (startWorkMinute === 0 && startWorkSeconds === 0) {
+                if (count === 0) {
+                    clearInterval(workTimer);
+                } else {
+                    $("#status").html('Break Time!');
+                    startWorkSeconds = startBreakSeconds;
+                    startWorkMinute = startBreakMinute;
+                    //DISPLAY INITIAL BREAK TIME
+                    if (startWorkMinute <= 9) {
+                        $("#work-minutes-display").html('0' + startWorkMinute);
+                    } else {
+                        $("#work-minutes-display").html(startWorkMinute);
+                    }
+                    if (startWorkSeconds <= 9) {
+                        $("#work-seconds-display").html('0' + startWorkSeconds);
+                    } else {
+                        $("#work-seconds-display").html(startWorkSeconds);
+                    }
+                    count -= 1;
+                }
             } else {
-                startBreakMinute -= 1;
-                $("#break-minutes-display").html(startBreakMinute);
+                startWorkSeconds = 59;
+                $("#work-seconds-display").html(startWorkSeconds);
+                if (startWorkMinute - 1 <= 9) {
+                    startWorkMinute -= 1;
+                    $("#work-minutes-display").html('0' + startWorkMinute);
+
+                } else {
+                    startWorkMinute -= 1;
+                    $("#work-minutes-display").html(startWorkMinute);
+                }
             }
         }
     }
-});
 
-/*Hi! I have an issue with my pomdoro clock, where the there is a slightly longer delay between when the work timer reaches 00:00 and when the break timer begins. (countdown is delayed with 1 second intervals otherwise). Any help would be grealty appreciated!
-http://codepen.io/Toe125/pen/pyLJyJ
-*/
+    var workTimer = setInterval(timer, 1000);
+
+    $('#pause').click(
+        function() {
+            isPaused = true;
+        });
+
+    $('#start').click(
+        function() {
+            isPaused = false;
+        });
+});
