3aac7b68d67b1c71622cc7524afccaf35a9f68c6
Toby Chow <toby_chow@hotmail.com>
2016-04-16 20:15:38 +0800

    complete all fcc requirements

--- timer.js
+++ timer.js
@@ -1,66 +1,105 @@
+//TODO: ALLOW USERS TO CHOOSE ALARM LENGTH, ADD OPTION FOR USER IF RESET = 25/5 OR CUSTOM
+
 $(document).ready(function() {
 
     // Initial work minutes / seconds
 
-    var startWorkMinute = 25;
-    var startWorkSeconds = 0;
+    var startWorkMinute = 0;
+    var startWorkSeconds = 4;
 
     // Initial break minutes / seconds
 
-    var startBreakMinute = 5;
-    var startBreakSeconds = 0;
-
-    // COUNT TO STOP LOOP AFTER BREAK TIMER ENDS
-    var count = 1;
+    var startBreakMinute = 0;
+    var startBreakSeconds = 4;
 
     // PAUSE
     var isPaused = true;
 
+    // STORE ALARM DOM
 
-    // ADD / MINUS INITIAL WORK/BREAK SESSION LENGTHS
+    var alarm = $('#alarm')[0];
 
-    $("#add-work-min").click(function() {
-        startWorkMinute += 1;
-        display();
-    });
+    // PLAY ALARM AUDIO FUNCTION
 
-    $("#minus-work-min").click(function() {
-        if ($(".start-work-display").html() <= 1) {
-            startWorkMinute -= 0;
-        } else {
-            startWorkMinute -= 1;
+    function playAlarm() {
+        alarm.currentTime = 0;
+        alarm.play();
+        setTimeout(function() { alarm.pause(); }, 3000);
+    }
+
+    // ENABLE ADDING/SUBTRACTING SESSION LENGTHS
+
+    function bindClick() {
+        $("#add-work-min").on("click", function() {
+            startWorkMinute += 1;
             display();
-        }
-    });
+        });
 
-    $("#add-break-min").click(function() {
-        startBreakMinute += 1;
-        display();
-    });
+        $("#minus-work-min").click(function() {
+            if ($(".start-work-display").html() <= 1) {
+                startWorkMinute -= 0;
+            } else {
+                startWorkMinute -= 1;
+                display();
+            }
+        });
 
-    $("#minus-break-min").click(function() {
-        if ($(".start-break-display").html() <= 1) {
-            startBreakMinute -= 0;
-        } else {
-            startBreakMinute -= 1;
+        $("#add-break-min").click(function() {
+            startBreakMinute += 1;
             display();
-        }
-    });
+        });
+
+        $("#minus-break-min").click(function() {
+            if ($(".start-break-display").html() <= 1) {
+                startBreakMinute -= 0;
+            } else {
+                startBreakMinute -= 1;
+                display();
+            }
+        });
+    }
+    bindClick();
+
+
+    // REMOVES ADD/MINUS FUNCTIONS (ARROWS DONT WORK)
+    function unbindClick() {
+        $(".start-time").find('span').off();
+    }
+
+    // START / PAUSE FUNCTIONS, PAUSECOUNT TO PREVENT BINDING CLICK FUNCTION MORE THAN ONCE
+    var pauseCount = 0;
+    $('#pause').click(
+        function() {
+            isPaused = true;
+            if (pauseCount >= 1) {
+                bindClick();
+            }
+            pauseCount = 0;
+        });
+
+    $('#start').click(
+        function() {
+            isPaused = false;
+            unbindClick();
+            pauseCount += 1;
+        });
 
     // RESET BUTTON 
 
     $("#reset").click(function() {
+        alarm.pause();
         $("#status").html('Work Time!');
         startWorkMinute = 25;
         startWorkSeconds = 0;
         startBreakMinute = 5;
         startBreakSeconds = 0;
         isPaused = true;
+        bindClick();
         display();
     });
 
-
     // DISPLAYS INITIAL WORK/BREAK SESSION AND INITIAL START TIME FOR COUNTDOWN TIMER
+
     function display() {
         if (startWorkMinute <= 9) {
             $("#work-minutes-display").html('0' + startWorkMinute);
@@ -82,10 +121,13 @@ $(document).ready(function() {
     }
     display();
 
+    // COUNT TO STOP LOOP AFTER BREAK TIMER ENDS
+    var count = 1;
 
     //COUNTDOWN TIMER FUNCTION
 
     function timer() {
+
         if (!isPaused) {
             if (startWorkSeconds - 1 >= 0) {
                 if (startWorkSeconds - 1 <= 9) {
@@ -98,10 +140,12 @@ $(document).ready(function() {
             } else if (startWorkMinute === 0 && startWorkSeconds === 0) {
                 if (count === 0) {
                     clearInterval(workTimer);
+                    playAlarm();
                 } else {
                     $("#status").html('Break Time!');
                     startWorkSeconds = startBreakSeconds;
                     startWorkMinute = startBreakMinute;
+                    playAlarm();
                     //DISPLAY INITIAL BREAK TIME
                     if (startWorkMinute <= 9) {
                         $("#work-minutes-display").html('0' + startWorkMinute);
@@ -132,13 +176,6 @@ $(document).ready(function() {
 
     var workTimer = setInterval(timer, 1000);
 
-    $('#pause').click(
-        function() {
-            isPaused = true;
-        });
 
-    $('#start').click(
-        function() {
-            isPaused = false;
-        });
+
 });
